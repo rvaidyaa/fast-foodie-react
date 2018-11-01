eed620deeac01fc3452e9806a39271f853a4d15d
rvaidyaa <rvaidyaa@gmail.com>
2018-11-01 15:45:50 -0700

    added axios and updated SearchBar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,27 @@ import './App.css';
 import SearchBar from "./components/SearchBar";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      restaurants: [],
+      zipcode: null
+    };
+    this.getzip = this.getzip.bind(this);
+    this.getWeatherAndRestaurants = this.getWeatherAndRestaurants.bind(this);
+  }
+  getzip(zip){
+    this.setState({zipcode:zip})
+  }
+  getWeatherAndRestaurants(zipcode){
+
+  }
   render() {
     return (
       <div className="App">
-        <SearchBar/>
+        <SearchBar onSearchTermChange={this.getzip} onSubmit={this.getWeatherAndRestaurants} />
+        
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,37 +1,52 @@
-import React, { Component } from 'react';
-import './SearchBar.css';
+import React, { Component } from "react";
+import "./SearchBar.css";
 
 export default class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      restaurants: [],
-      zipcode: ""
+      zipcode: null
     };
-
-    
+    this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
   }
+  handleChange(event) {
+    this.setState({ zipcode: event.target.value });
+    this.props.onSearchTermChange(this.state.zipcode);
+  }
+  // handleSubmit(event){
+  //   this.props.
+  // }
+
   render() {
     return (
       <div>
-        <main role='main'>
-        <section className='open-screen'>
+        <main role="main">
+          <section className="open-screen">
             <h1>Fast Foodie</h1>
-            <h2>Input your Zip Code in order to get the 9 top restaurants in your area and weather to plan a quick meal out!</h2>
-            <form id='form'>
-                <input type='text' placeholder="ZipCode" className='user-input' /><br/>
-
-                <input className='search-button' type='submit' value='Search'/>
+            <h2>
+              Input your Zip Code in order to get the 9 top restaurants in your
+              area and weather to plan a quick meal out!
+            </h2>
+            <form id="form">
+              <input
+                placeholder="ZipCode"
+                className="user-input"
+                value={this.state.zipcode}
+                onChange={this.handleChange}
+              />
+              <br />
+              
+              <input className="search-button" type="submit" value="Search" />
             </form>
-        </section>
-        <div className='weather-results'>
-            <ul></ul>
-        </div>
-        <section role='wrapper' className='results-wrapper'>
-        </section>
-    </main>
+          </section>
+          <div className="weather-results">
+            <ul />
+          </div>
+          <section role="wrapper" className="results-wrapper" />
+        </main>
       </div>
-    )
+    );
   }
 }
